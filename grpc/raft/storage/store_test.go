cc830d102d724110bb25ad2463ec36763358e629
Gabriel Leon <gleon@godaddy.com>
2026-05-05 13:07:08 -0300

    feat: add storage

---
+++ grpc/raft/storage/store_test.go
@@ -0,0 +1,61 @@
+package storage
+
+import (
+	"raft/models/node"
+	"testing"
+)
+
+func TestKeyValueStoreGet(t *testing.T) {
+	store := KeyValueStore
+	store.set("key1", "value1")
+
+	val, ok := store.Get("key1")
+	if !ok {
+		t.Errorf("expected key to exist")
+	}
+	if val != "value1" {
+		t.Errorf("expected value to be 'value1', got '%s'", val)
+	}
+}
+
+func TestKeyValueStoreApplySet(t *testing.T) {
+	store := KeyValueStore
+	cmd := node.Command{
+		Type:  node.SET,
+		Key:   "key2",
+		Value: func() *string { v := "value2"; return &v }(),
+	}
+
+	err := store.Apply(cmd)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	val, ok := store.Get("key2")
+	if !ok {
+		t.Errorf("expected key to exist")
+	}
+	if val != "value2" {
+		t.Errorf("expected value to be 'value2', got '%s'", val)
+	}
+}
+
+func TestKeyValueStoreApplyDelete(t *testing.T) {
+	store := KeyValueStore
+	store.set("key3", "value3")
+
+	cmd := node.Command{
+		Type: node.DELETE,
+		Key:  "key3",
+	}
+
+	err := store.Apply(cmd)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	_, ok := store.Get("key3")
+	if ok {
+		t.Errorf("expected key to be deleted")
+	}
+}
